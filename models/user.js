6ec25005533b9b426321c78e243645dac3c652c7
m_mitsuhide <m.mitsuhide@amatelus.com>
2018-08-20 03:36:31 +0900

    add: mongodb setup

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const moment = require('moment');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const createHash = require(`${process.cwd()}/utils/createHash.js`);
+
+const UserSchema = new Schema({
+  user_id: String,
+  name: String,
+  email: String,
+  password: String,
+  created: String,
+});
+
+UserSchema.methods.setInitParams = function() {
+  this.user_id = createHash(8);
+  this.created = moment().format("YYYY-MM-DD HH:mm:ss"); 
+};
+
+module.exports = mongoose.model('User', UserSchema);
--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.use('/test', require('./test.js'));
+router.use('/users', require('./users.js'));
 
 module.exports = router;
