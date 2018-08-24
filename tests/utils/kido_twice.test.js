a76996763545485d5a82d925bf35d356a579775c
DESKTOP-Q7A8B81\kidok <kotaro.k13@gmail.com>
2018-08-25 04:36:04 +0900

    add: kido.js

---
+++ tests/utils/kido_twice.test.js
@@ -0,0 +1,11 @@
+const twice = require('../../src/utils/kido_twice.js');
+
+describe('Twice number', () => {
+    it('3 × 2 = 6', () => 20);
+    const result = twice(3);
+    expect(result) = twice(3);
+
+    it('5.5 × 11 = 6', () => 20);
+    const result = twice(3);
+    expect(result) = twice(3);
+});
