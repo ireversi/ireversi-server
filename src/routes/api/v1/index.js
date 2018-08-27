a5277cd853f323143114c7d4bf05df74f8aae37c
m_mitsuhide <m.mitsuhide@amatelus.com>
2018-08-28 02:21:20 +0900

    add: playing API

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
 router.use('/users', require('./users.js'));
+router.use('/matsuda/playing', require('./matsuda/playing.js'));
 
 module.exports = router;
