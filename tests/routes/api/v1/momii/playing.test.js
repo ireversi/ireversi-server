81084918410eb6c61cf74958840053a8e11b1884
Momii Yusuke <momiiyusuke@team-lab.com>
2018-08-29 02:19:03 +0900

    [feat] test用jsとmodelを追加

---
+++ tests/routes/api/v1/momii/playing.test.js
@@ -0,0 +1,83 @@
+const chai = require('chai');
+
+const app = require('../../../../../src/routes/app');
+const PlayingModel = require('../../../../../src/models/momii/PlayingModel');
+
+const {
+  prepareDB,
+  deleteAllDataFromDB,
+} = require('../../../../../src/utils/db.js');
+
+const basePath = '/api/v1';
+
+describe('Show Board', () => {
+  beforeAll(prepareDB);
+  afterEach(deleteAllDataFromDB);
+
+  describe('show', () => {
+    it('shows an Board', async () => {
+      // Given
+      const x = 1;
+      const y = 20;
+      const userId = 3;
+      const Playing = new PlayingModel({
+        x,
+        y,
+        userId,
+      });
+      Playing.setInitParams();
+      await Playing.save();
+
+      // When
+      const response = await chai.request(app)
+        .get(`${basePath}/momii/playing`)
+        .query({ x, y, userId });
+      // Then
+      expect(response.body).toMatchObject({
+        x: expect.any(Number),
+        y: expect.any(Number),
+        userId: expect.any(Number),
+      });
+    });
+
+    it('return null when the user does not exist', async () => {
+      // Given
+      const x = 88888;
+      const y = 88888;
+      const userId = 88888;
+
+      // When
+      const response = await chai.request(app)
+        .get(`${basePath}/momii/playing`)
+        .query({ x, y, userId });
+      // Then
+      expect(response.body).toBe(null);
+    });
+  });
+
+  describe('create', () => {
+    it('creates an user', async () => {
+      // Given
+      const x = 1;
+      const y = 20;
+      const userId = 3;
+
+      // When
+      const userMatcher = {
+        x,
+        y,
+        userId,
+      };
+
+      const response = await chai.request(app)
+        .post(`${basePath}/momii/playing`)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(userMatcher);
+
+      // Then
+      expect(response.body).toMatchObject({ status: 'success' });
+      const playing = await PlayingModel.findOne({ x, y, userId });
+      expect(playing).toMatchObject(userMatcher);
+    });
+  });
+});
