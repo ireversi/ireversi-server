e4f8e93c724ee2e6f56bad51893535eb4719a011
Shohei Kai <kai.entertainment0304@gmail.com>
2018-09-01 03:53:40 +0900

    add: config

--- src/config.js
+++ src/config.js
@@ -1,24 +1,5 @@
 const dotenv = require('dotenv');
-<<<<<<< HEAD
-
-// 環境変数
-// 環境が変わったら変わるもの
-// PC・場所・ユーザーなど
-
-switch (process.env.NODE_ENV) { // 読み込み先を変える
-  case 'production':
-    break;
-  case 'test':
-    dotenv.config({
-      path: 'test.env',
-    });
-    break;
-  default:
-    dotenv.config();
-}
-=======
 dotenv.config();
->>>>>>> 9d731ff8d88a2e6bb34a6564dd04a5b4f331c1ef
 
 module.exports = { //環境変数
   nodeEnv: process.env.NODE_ENV || 'development',
