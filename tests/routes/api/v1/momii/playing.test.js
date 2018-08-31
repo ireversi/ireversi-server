40281e3467a86a1e25d309adaeccac5e62a77811
Momii Yusuke <momiiyusuke@team-lab.com>
2018-09-01 02:02:04 +0900

    [fix] playing.test.jsの修正

--- tests/routes/api/v1/momii/playing.test.js
+++ tests/routes/api/v1/momii/playing.test.js
@@ -1,16 +1,12 @@
 const chai = require('chai');
 
 const app = require('../../../../../src/routes/app');
-const PlayingModel = require('../../../../../src/models/momii/PlayingModel');
+const PlayingModel = require('../../../../../src/models/momii/PlayingModel.js');
+
 const {
   prepareDB,
   deleteAllDataFromDB,
 } = require('../../../../../src/utils/db.js');
-
-// const {
-//   prepareDB,
-//   deleteAllDataFromDB,
-// } = require('../../../../../src/utils/db.js');
 
 const basePath = '/api/v1';
 
@@ -26,6 +22,9 @@ describe('Play', () => {
       userId: 1,
     };
 
+    const Playing = new PlayingModel(piece);
+    await Playing.save();
+
     // when
     const response = await chai.request(app)
       .post(`${basePath}/momii/playing`)
@@ -34,58 +33,10 @@ describe('Play', () => {
 
     // then
     expect(response.body).toHaveLength(1);
-    expect(response.body).toMatchObject([piece]);
+    expect(response.body[0]).toMatchObject(piece);
 
-    const pieces = await PlayingModel.find();
-    // expect(pieces).
+    const pieces = await PlayingModel.find({});
+    expect(pieces).toHaveLength(1);
+    expect(pieces[0]).toMatchObject(piece);
   });
-
-  // describe('show', () => {
-  //   it('shows an Board', async () => {
-  //     // Given
-  //     const x = 0;
-  //     const y = 0;
-  //     const userId = 1;
-  //     const Playing = new PlayingModel({
-  //       x,
-  //       y,
-  //       userId,
-  //     });
-  //     // Playing.setInitParams();
-  //     await Playing.save();
-  //
-  //     // When
-  //     const response = await chai.request(app)
-  //       .get(`${basePath}/momii/playing`)
-  //       .query({ x, y, userId });
-  //     // Then
-  //     expect({
-  //       x,
-  //       y,
-  //       userId,
-  //     }).toMatchObject({
-  //       x: expect.any(Number),
-  //       y: expect.any(Number),
-  //       userId: expect.any(Number),
-  //     });
-  //   });
-  //
-  //   it('return null when the user does not exist', async () => {
-  //     // Given
-  //     const x = 88888;
-  //     const y = 88888;
-  //     const userId = 88888;
-  //
-  //     // When
-  //     const response = await chai.request(app)
-  //       .get(`${basePath}/momii/playing`)
-  //       .query({ x, y, userId });
-  //     // Then
-  //     expect({
-  //       x,
-  //       y,
-  //       userId,
-  //     }).toMatchObject({});
-  //   });
-  // });
 });
