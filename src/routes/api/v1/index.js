739df3279d1a2708268244e3f7da25e480da20b0
Yohei <yohei.fujii1127@gmail.com>
2018-09-01 01:47:27 +0900

    add playing API

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,5 +1,13 @@
-const router = require('express').Router();
+// const router = require('express').Router();
+
+// router.use('/users', require('./users.js'));
+
+// module.exports = router;
+
+
+const router = require('express').Router(); //ルーターの役割
 
 router.use('/users', require('./users.js'));
+router.use('/homework/playing', require('./homework/playing.js'));
 
 module.exports = router;
--- src/routes/app.js
+++ src/routes/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));//use:他のモジュールから飛んでくる情報をparseする
 app.use('/api/v1', require('./api/v1/index.js'));
 
 module.exports = app;
