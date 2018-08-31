816fd57910f254e6589d5247e314cc2bf1c699ad
hi85 <33993836+hi85gh@users.noreply.github.com>
2018-08-31 22:58:39 +0900

    Add scripts (Replace sample script)

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.use('/users', require('./users.js'));
 router.use('/matsuda/playing', require('./matsuda/playing.js'));
+router.use('/hi85/playing', require('./hi85/playing.js'));
 
 module.exports = router;
