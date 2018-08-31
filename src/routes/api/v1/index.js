0bf557f1b4c3dac6b7bb24eb4c287d642964f58f
hideto2112 <e10005@shibaura-it.ac.jp>
2018-09-01 02:45:58 +0900

    Add routes for index.js

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -4,5 +4,7 @@ router.use('/users', require('./users.js'));
 router.use('/matsuda/playing', require('./matsuda/playing.js'));
 router.use('/hi85/playing', require('./hi85/playing.js'));
 router.use('/momii/playing', require('./momii/playing.js'));
+router.use('/ando/playing', require('./ando/playing.js'));
+router.use('/ando/piece', require('./ando/piece.js'));
 
 module.exports = router;
