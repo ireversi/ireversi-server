9b1bfaf1c265cb301fd641605146bdad43137a91
DESKTOP-Q7A8B81\kidok <kotaro.k13@gmail.com>
2018-09-03 05:16:52 +0900

    modify for lint check

--- src/routes/api/v1/kido_k/piece.js
+++ src/routes/api/v1/kido_k/piece.js
@@ -1,22 +1,62 @@
 const router = require('express').Router();
 const PieceModel = require('../../../../models/kido_k/PieceModel.js');
+
 const propfilter = '-_id -__v';
+
+function sortList(list, sort) {
+  if (sort.x === 'asc' && sort.y === 'asc') {
+    list.sort((a, b) => {
+      if (a.x < b.x) return -1;
+      if (a.x > b.x) return 1;
+      if (a.y < b.y) return -1;
+      if (a.y > b.y) return 1;
+      return 0;
+    });
+  } else if (sort.x === 'asc' && sort.y === 'desc') {
+    list.sort((a, b) => {
+      if (a.x < b.x) return -1;
+      if (a.x > b.x) return 1;
+      if (a.y < b.y) return 1;
+      if (a.y > b.y) return -1;
+      return 0;
+    });
+  } else if (sort.x === 'desc' && sort.y === 'asc') {
+    list.sort((a, b) => {
+      if (a.x < b.x) return 1;
+      if (a.x > b.x) return -1;
+      if (a.y < b.y) return -1;
+      if (a.y > b.y) return 1;
+      return 0;
+    });
+  } else if (sort.x === 'desc' && sort.y === 'desc') {
+    list.sort((a, b) => {
+      if (a.x < b.x) return 1;
+      if (a.x > b.x) return -1;
+      if (a.y < b.y) return 1;
+      if (a.y > b.y) return -1;
+      return 0;
+    });
+  } else {
+    console.log('need setting sort param');
+  }
+  return list;
+}
 
 router.route('/')
   .post(async (req, res) => {
     const pieces = await PieceModel.find({}, propfilter);
     const result = {
-      x: + req.body.x,
-      y: + req.body.y,
-      userid: + req.body.userid,
+      x: +req.body.x,
+      y: +req.body.y,
+      userid: +req.body.userid,
     };
 
-    // check puts piece same space 
+    // check puts piece same space
     // ============================================================
     if (pieces.find(p => p.x === result.x && p.y === result.y)) {
       res.json(pieces);
       return;
-    };
+    }
     const Piece = new PieceModel(result);
     await Piece.save();
 
@@ -24,18 +64,20 @@ router.route('/')
 
     // check turn over
     // ============================================================
-    //pick up 
-    const turnlist = { n: [], ne: [], e: [], se: [], s: [], sw: [], w: [], nw: [] }
-    for (var i = 0; i < pieces.length; i++) {
+    // pick up
+    const turnlist = {
+      n: [], ne: [], e: [], se: [], s: [], sw: [], w: [], nw: [],
+    };
+    for (let i = 0; i < pieces.length; i += 1) {
       const piece = pieces[i];
 
       if (piece.x === result.x && piece.y < result.y && piece.userid === result.userid) {
         turnlist.n.push(piece);
       } else if (piece.x > result.x && piece.y === result.y && piece.userid === result.userid) {
-        console.log("east");
+        console.log('east');
         turnlist.e.push(piece);
       } else if (piece.x === result.x && piece.y > result.y && piece.userid === result.userid) {
-        console.log("south");
+        console.log('south');
         turnlist.s.push(piece);
       } else if (piece.x < result.x && piece.y === result.y && piece.userid === result.userid) {
         turnlist.w.push(piece);
@@ -50,49 +92,47 @@ router.route('/')
         //   turnlist.sw.push(piece);
         // } else if (piece.x < result.x && piece.y > result.y && piece.userid !== 0) {
         //   turnlist.nw.push(piece);
-      } else {
-
       }
     }
 
     if (turnlist.n.length !== 0) {
       const sort = { x: 'asc', y: 'desc' };
       turnlist.n = sortList(turnlist.n, sort);
-      for (var i = result.y - 1; i > turnlist.n[0].y; i--) {
-        await PieceModel.remove({ "x": + turnlist.n[0].x, "y": + i });
-        const Piece = new PieceModel({ x: turnlist.n[0].x, y: i, userid: result.userid });
-        await Piece.save();
-      };
+      for (let i = result.y - 1; i > turnlist.n[0].y; i -= 1) {
+        await PieceModel.remove({ x: +turnlist.n[0].x, y: +i });
+        const PieceN = new PieceModel({ x: turnlist.n[0].x, y: i, userid: result.userid });
+        await PieceN.save();
+      }
     }
 
     if (turnlist.e.length !== 0) {
       const sort = { x: 'asc', y: 'asc' };
       turnlist.e = sortList(turnlist.e, sort);
-      for (var i = result.x + 1; i < turnlist.e[0].x; i++) {
-        await PieceModel.remove({ "x": + i, "y": + turnlist.e[0].y })
-        const Piece = new PieceModel({ x: i, y: turnlist.e[0].y, userid: result.userid });
-        await Piece.save();
-      };
+      for (let i = result.x + 1; i < turnlist.e[0].x; i += 1) {
+        await PieceModel.remove({ x: +i, y: +turnlist.e[0].y });
+        const PieceE = new PieceModel({ x: i, y: turnlist.e[0].y, userid: result.userid });
+        await PieceE.save();
+      }
     }
 
     if (turnlist.s.length !== 0) {
       const sort = { x: 'asc', y: 'asc' };
       turnlist.s = sortList(turnlist.s, sort);
-      for (var i = result.y + 1; i < turnlist.s[0].y; i++) {
-        await PieceModel.remove({ "x": + turnlist.s[0].x, "y": + i });
-        const Piece = new PieceModel({ x: turnlist.s[0].x, y: i, userid: result.userid });
-        await Piece.save();
-      };
+      for (let i = result.y + 1; i < turnlist.s[0].y; i += 1) {
+        await PieceModel.remove({ x: +turnlist.s[0].x, y: +i });
+        const PieceS = new PieceModel({ x: turnlist.s[0].x, y: i, userid: result.userid });
+        await PieceS.save();
+      }
     }
 
     if (turnlist.w.length !== 0) {
       const sort = { x: 'desc', y: 'asc' };
       turnlist.w = sortList(turnlist.w, sort);
-      for (var i = result.x - 1; i > turnlist.w[0].x; i--) {
-        await PieceModel.remove({ "x": + i, "y": + turnlist.w[0].y });
-        const Piece = new PieceModel({ x: i, y: turnlist.w[0].y, userid: result.userid });
-        await Piece.save();
-      };
+      for (let i = result.x - 1; i > turnlist.w[0].x; i -= 1) {
+        await PieceModel.remove({ x: +i, y: +turnlist.w[0].y });
+        const PieceW = new PieceModel({ x: i, y: turnlist.w[0].y, userid: result.userid });
+        await PieceW.save();
+      }
     }
     // if (turnlist.ne.length !== 0) {
     //   turnlist.ne = sortList(turnlist.ne);
@@ -110,43 +150,3 @@ router.route('/')
   });
 
 module.exports = router;
-
-function sortList(list, sort) {
-
-  if (sort.x === 'asc', sort.y === 'asc') {
-    list.sort(function (a, b) {
-      if (a.x < b.x) return -1;
-      if (a.x > b.x) return 1;
-      if (a.y < b.y) return -1;
-      if (a.y > b.y) return 1;
-      return 0;
-    });
-  } else if (sort.x === 'asc', sort.y === 'desc') {
-    list.sort(function (a, b) {
-      if (a.x < b.x) return -1;
-      if (a.x > b.x) return 1;
-      if (a.y < b.y) return 1;
-      if (a.y > b.y) return -1;
-      return 0;
-    });
-  } else if (sort.x === 'desc', sort.y === 'asc') {
-    list.sort(function (a, b) {
-      if (a.x < b.x) return 1;
-      if (a.x > b.x) return -1;
-      if (a.y < b.y) return -1;
-      if (a.y > b.y) return 1;
-      return 0;
-    });
-  } else if (sort.x === 'desc', sort.y === 'desc') {
-    list.sort(function (a, b) {
-      if (a.x < b.x) return 1;
-      if (a.x > b.x) return -1;
-      if (a.y < b.y) return 1;
-      if (a.y > b.y) return -1;
-      return 0;
-    });
-  } else {
-    console.log("need setting sort param");
-  }
-  return list;
-}
--- tests/routes/api/v1/kido_k/piece.test.js
+++ tests/routes/api/v1/kido_k/piece.test.js
@@ -1,4 +1,5 @@
 const chai = require('chai');
+
 const propfilter = '-_id -__v';
 // module.exports = () => {
 
@@ -10,6 +11,29 @@ const propfilter = '-_id -__v';
 
 // };
 // ES6 import
+
+function convertPiece(piece) {
+  const convert = { x: piece[0], y: piece[1], userid: piece[2] };
+  return convert;
+}
+
+function convertComparisonResult(result) {
+  // var count = 0;
+  const pieces = [];
+  const bsize = result.size;
+  const bpieces = result.bord;
+  for (let i = 0; i < bpieces.length; i += 1) {
+    if (bpieces[i] !== 0) {
+      const piece = {
+        x: Math.floor(i % bsize),
+        y: Math.floor(i / bsize),
+        userid: bpieces[i],
+      };
+      pieces.push(piece);
+    }
+  }
+  return pieces;
+}
 
 const app = require('../../../../../src/routes/app.js');
 const PieceModel = require('../../../../../src/models/kido_k/PieceModel.js');
@@ -31,16 +55,16 @@ describe('play', () => {
     ];
 
     const result = {
-      size: 2,    //set width of bord
-      bord: [     //set correct answer
+      size: 2, // set width of bord
+      bord: [ // set correct answer
         1, 0,
-        0, 0
-      ]
+        0, 0,
+      ],
     };
 
     // When
-    var response;
-    for (var i = 0; i < pieces.length; i++) {
+    let response;
+    for (let i = 0; i < pieces.length; i += 1) {
       const piece = convertPiece(pieces[i]);
       response = await chai.request(app)
         .post(`${basePath}/kido_k/piece`)
@@ -63,20 +87,20 @@ describe('play', () => {
     // Given
     const pieces = [ // pieces = [x,y,userid]
       [0, 0, 1],
-      [1, 0, 2]
+      [1, 0, 2],
     ];
 
     const result = {
-      size: 2,    //set width of bord
-      bord: [     //set correct answer
+      size: 2, // set width of bord
+      bord: [ // set correct answer
         1, 2,
-        0, 0
-      ]
+        0, 0,
+      ],
     };
 
     // When
-    var response;
-    for (var i = 0; i < pieces.length; i++) {
+    let response;
+    for (let i = 0; i < pieces.length; i += 1) {
       const piece = convertPiece(pieces[i]);
       response = await chai.request(app)
         .post(`${basePath}/kido_k/piece`)
@@ -94,25 +118,25 @@ describe('play', () => {
     expect(pieceData).toEqual(expect.arrayContaining(rpieces));
   });
 
-  //同じとFころにおけないテスト
+  // 同じとFころにおけないテスト
   it('sets pieces same space', async () => {
     // Given
     const pieces = [ // pieces = [x,y,userid]
       [0, 0, 1],
       [1, 0, 2],
-      [1, 0, 1]
+      [1, 0, 1],
     ];
 
     const result = {
-      size: 2,    //set width of bord
-      bord: [     //set correct answer
+      size: 2, // set width of bord
+      bord: [ // set correct answer
         1, 2,
-        0, 0
-      ]
+        0, 0,
+      ],
     };
 
     // When
-    for (var i = 0; i < pieces.length; i++) {
+    for (let i = 0; i < pieces.length; i += 1) {
       const piece = convertPiece(pieces[i]);
       response = await chai.request(app)
         .post(`${basePath}/kido_k/piece`)
@@ -121,19 +145,18 @@ describe('play', () => {
     }
 
     // Then
-    //check express result
+    // check express result
     const rpieces = convertComparisonResult(result);
     expect(response.body).toHaveLength(rpieces.length);
     expect(response.body).toEqual(expect.arrayContaining(rpieces));
 
-    //check mongoDB result
+    // check mongoDB result
     const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
     expect(pieceData).toHaveLength(rpieces.length);
     expect(pieceData).toEqual(expect.arrayContaining(rpieces));
-
   });
 
-  //挟んだらめくれるテスト part1
+  // 挟んだらめくれるテスト part1
   it('turn over piece about right and down', async () => {
     // Given
     const pieces = [ // pieces = [x,y,userid]
@@ -145,16 +168,16 @@ describe('play', () => {
     ];
 
     const result = {
-      size: 3,    //set width of bord
-      bord: [     //set correct answer
+      size: 3, // set width of bord
+      bord: [ // set correct answer
         1, 1, 1,
         1, 0, 0,
-        1, 0, 0
-      ]
+        1, 0, 0,
+      ],
     };
 
     // When
-    for (var i = 0; i < pieces.length; i++) {
+    for (let i = 0; i < pieces.length; i += 1) {
       const piece = convertPiece(pieces[i]);
       response = await chai.request(app)
         .post(`${basePath}/kido_k/piece`)
@@ -163,18 +186,18 @@ describe('play', () => {
     }
 
     // Then
-    //check express result
+    // check express result
     const rpieces = convertComparisonResult(result);
     expect(response.body).toHaveLength(rpieces.length);
     expect(response.body).toEqual(expect.arrayContaining(rpieces));
 
-    //check mongoDB result
+    // check mongoDB result
     const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
     expect(pieceData).toHaveLength(rpieces.length);
     expect(pieceData).toEqual(expect.arrayContaining(rpieces));
   });
 
-  //挟んだらめくれるテスト part2
+  // 挟んだらめくれるテスト part2
   it('turn over piece about left and up', async () => {
     // Given
     const pieces = [ // pieces = [x,y,userid]
@@ -186,16 +209,16 @@ describe('play', () => {
     ];
 
     const result = {
-      size: 3,    //set width of bord
-      bord: [     //set correct answer
+      size: 3, // set width of bord
+      bord: [ // set correct answer
         0, 0, 1,
         0, 0, 1,
-        1, 1, 1
-      ]
+        1, 1, 1,
+      ],
     };
 
     // When
-    for (var i = 0; i < pieces.length; i++) {
+    for (let i = 0; i < pieces.length; i += 1) {
       const piece = convertPiece(pieces[i]);
       response = await chai.request(app)
         .post(`${basePath}/kido_k/piece`)
@@ -204,38 +227,14 @@ describe('play', () => {
     }
 
     // Then
-    //check express result
+    // check express result
     const rpieces = convertComparisonResult(result);
     expect(response.body).toHaveLength(rpieces.length);
     expect(response.body).toEqual(expect.arrayContaining(rpieces));
 
-    //check mongoDB result
+    // check mongoDB result
     const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
     expect(pieceData).toHaveLength(rpieces.length);
     expect(pieceData).toEqual(expect.arrayContaining(rpieces));
   });
-
 });
-
-function convertPiece(piece) {
-  var convert = { x: piece[0], y: piece[1], userid: piece[2] }
-  return convert;
-}
-
-function convertComparisonResult(result) {
-  // var count = 0;
-  const pieces = [];
-  const bsize = result.size;
-  const bpieces = result.bord;
-  for (var i = 0; i < bpieces.length; i++) {
-    if (bpieces[i] !== 0) {
-      const piece = {
-        x: Math.floor(i % bsize),
-        y: Math.floor(i / bsize),
-        userid: bpieces[i]
-      }
-      pieces.push(piece);
-    }
-  }
-  return pieces;
-}
