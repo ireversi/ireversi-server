a3b5ba1c7f9b04eb62dc4958a69533f882cb7436
DESKTOP-Q7A8B81\kidok <kotaro.k13@gmail.com>
2018-09-04 04:06:52 +0900

    modify test param format

--- tests/routes/api/v1/kido_k/piece.test.js
+++ tests/routes/api/v1/kido_k/piece.test.js
@@ -24,14 +24,15 @@ const basePath = '/api/v1';
 
 function convert2PieceRecord(pieces) {
   const record = [];
-  for (let i = 0; i < pieces.record.length; i += 1) {
-    const piece = pieces.record[i];
+  for (let i = 0; i < pieces.length; i += 1) {
+    const size = Math.sqrt(pieces.length)
+    const piece = pieces[i];
     if (piece !== 0 && !Array.isArray(piece)) {
       const point = piece.indexOf(':');
       const num = piece.slice(0, point);
       const userid = piece.slice(point + 1);
-      const x = Math.floor(i % pieces.size);
-      const y = Math.floor(i / pieces.size);
+      const x = Math.floor(i % size);
+      const y = Math.floor(i / size);
       record.push([num, x, y, userid]);
     } else if (piece !== 0 && Array.isArray(piece)) {
       for (let j = 0; j < piece.length; j += 1) {
@@ -39,8 +40,8 @@ function convert2PieceRecord(pieces) {
         const point = pie.indexOf(':');
         const num = pie.slice(0, point);
         const userid = pie.slice(point + 1);
-        const x = Math.floor(i % pieces.size);
-        const y = Math.floor(i / pieces.size);
+        const x = Math.floor(i % size);
+        const y = Math.floor(i / size);
         record.push([num, x, y, userid]);
       }
     }
@@ -60,14 +61,13 @@ function convertPiece(piece) {
 
 function convertComparisonResult(result) {
   const pieces = [];
-  const bsize = result.size;
-  const bpieces = result.bord;
-  for (let i = 0; i < bpieces.length; i += 1) {
-    if (bpieces[i] !== 0) {
+  const size = Math.sqrt(result.length)
+  for (let i = 0; i < result.length; i += 1) {
+    if (result[i] !== 0) {
       const piece = {
-        x: Math.floor(i % bsize),
-        y: Math.floor(i / bsize),
-        userid: bpieces[i],
+        x: Math.floor(i % size),
+        y: Math.floor(i / size),
+        userid: result[i],
       };
       pieces.push(piece);
     }
@@ -82,21 +82,15 @@ describe('play', () => {
 
   it('put a piece', async () => {
     // Given
-    const pieces = { // pieces = [x,y,userid]
-      size: 2, // set width of bord
-      record: [
-        0, 0,
-        '1:1', 0,
-      ],
-    };
+    const pieces = [
+      0, 0,
+      '1:1', 0,
+    ];
 
-    const result = {
-      size: 2, // set width of bord
-      bord: [ // set correct answer
-        0, 0,
-        1, 0,
-      ],
-    };
+    const result = [ // set correct answer
+      0, 0,
+      1, 0,
+    ];
 
     // When
     let response;
@@ -124,21 +118,15 @@ describe('play', () => {
 
   it('sets multi pieces', async () => {
     // Given
-    const pieces = {
-      size: 2, // set width of record
-      record: [
-        '1:1', '2:2',
-        0, 0,
-      ],
-    };
+    const pieces = [
+      '1:1', '2:2',
+      0, 0,
+    ];
 
-    const result = {
-      size: 2, // set width of bord
-      bord: [ // set correct answer
-        1, 2,
-        0, 0,
-      ],
-    };
+    const result = [ // set correct answer
+      1, 2,
+      0, 0,
+    ];
 
     // When
     let response;
@@ -165,23 +153,17 @@ describe('play', () => {
   // 同じとFころにおけないテスト
   it('sets pieces same space', async () => {
     // Given
-    const pieces = {
-      size: 2, // set width of record
-      record: [
-        '1:1', ['2:2', '3:1'],
-        0, 0,
-      ],
-    };
+    const pieces = [
+      '1:1', ['2:2', '3:1'],
+      0, 0,
+    ];
 
-    const result = {
-      size: 2, // set width of bord
-      bord: [ // set correct answer
-        1, 2,
-        0, 0,
-      ],
-    };
+    const result = [ // set correct answer
+      1, 2,
+      0, 0,
+    ];
 
-    //   // When
+    // When
     let response;
     const record = convert2PieceRecord(pieces);
     for (let i = 0; i < record.length; i += 1) {
@@ -208,23 +190,17 @@ describe('play', () => {
   // 挟んだらめくれるテスト part1
   it('turn over piece about right and down', async () => {
     // Given
-    const pieces = {
-      size: 3, // set width of record
-      record: [
-        '1:1', '2:2', '3:1',
-        '4:2', 0, 0,
-        '5:1', 0, 0,
-      ],
-    };
+    const pieces = [
+      '1:1', '2:2', '3:1',
+      '4:2', 0, 0,
+      '5:1', 0, 0,
+    ];
 
-    const result = {
-      size: 3, // set width of bord
-      bord: [ // set correct answer
-        1, 1, 1,
-        1, 0, 0,
-        1, 0, 0,
-      ],
-    };
+    const result = [
+      1, 1, 1,
+      1, 0, 0,
+      1, 0, 0,
+    ];
 
     // When
     let response;
@@ -254,23 +230,17 @@ describe('play', () => {
   // // 挟んだらめくれるテスト part2
   it('turn over piece about left and up', async () => {
     // Given
-    const pieces = {
-      size: 3, // set width of record
-      record: [
-        0, 0, '5:1',
-        0, 0, '4:2',
-        '3:1', '2:2', '1:1',
-      ],
-    };
+    const pieces = [
+      0, 0, '5:1',
+      0, 0, '4:2',
+      '3:1', '2:2', '1:1',
+    ];
 
-    const result = {
-      size: 3, // set width of bord
-      bord: [ // set correct answer
-        0, 0, 1,
-        0, 0, 1,
-        1, 1, 1,
-      ],
-    };
+    const result = [
+      0, 0, 1,
+      0, 0, 1,
+      1, 1, 1,
+    ];
 
     // When
     let response;
