26a507a1aff34ac690b9af3c6ea5bf875475f0ab
m_mitsuhide <m.mitsuhide@amatelus.com>
2018-09-04 19:18:13 +0900

    fix: lern option of mongoose

--- tests/routes/api/v1/matsuda/testUtils/getPiecesFromDB.js
+++ tests/routes/api/v1/matsuda/testUtils/getPiecesFromDB.js
@@ -1,5 +1,4 @@
 const PlayingModel = require('../../../../../../src/models/matsuda/PlayingModel.js');
 
 const propFilter = '-_id -__v';
-// eslint-disable-next-line max-len
-module.exports = async () => JSON.parse(JSON.stringify(await PlayingModel.find({}, propFilter).exec()));
+module.exports = () => PlayingModel.find({}, propFilter).lean();
