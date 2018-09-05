cab0db16399efdf9fd780b1941fd42fb200ed49e
Yohei <yohei.fujii1127@gmail.com>
2018-09-05 10:55:59 +0900

    task second done

--- tests/routes/api/v1/homework/playing.test.js
+++ tests/routes/api/v1/homework/playing.test.js
@@ -15,13 +15,21 @@ function reformPiece(d){
   var arry = [...Array(d.length).fill(0)];
   var r = Math.sqrt(d.length);
   for (var i = 0;i<d.length;i+=1){
-    if (d[i] !== 0){
+    if (d[i] !== 0 && !Array.isArray(d[i])){
       var obj = {
         x:i%r,
         y:r-Math.floor(i/r)-1,
         userId: +d[i].split(":")[0]
       }
       var order = +d[i].split(":")[1]-1
+      arry.splice(order,1,obj);
+    } else if (d[i] !== 0 && Array.isArray(d[i])){//配列が入っている場合 = 同じ手がある場合
+      var obj = {
+        x:i%r,
+        y:r-Math.floor(i/r)-1,
+        userId: +d[i][0].split(":")[0]
+      }
+      var order = +d[i][0].split(":")[1]-1
       arry.splice(order,1,obj);
     }
   }
@@ -63,6 +71,40 @@ describe('play', () => {
       // When
       let response;
       var rPiece = reformPiece(piece)
+      for (let i = 0; i < rPiece.length; i+=1) {
+        response = await chai.request(app)
+        .post(`${basePath}/homework/playing`)
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(rPiece[i]);
+      }
+
+
+      // // Then
+      var rMatchers = reformMatchers(matchers);
+      expect(response.body).toHaveLength(matchers.length);
+      expect(response.body).toEqual(expect.arrayContaining(rMatchers));
+
+      const pieces = JSON.parse(JSON.stringify(await PlayingModel.find({}, propfilter)))
+      expect(pieces).toHaveLength(matchers.length);
+      expect(pieces).toEqual(expect.arrayContaining(rMatchers));
+    });
+
+    it('puts on same the place', async () => {
+
+      const piece = [
+        '2:4',['1:3','2:5'],
+        '1:1','2:2' 
+      ];
+
+      const matchers = [
+        2,1,
+        1,2
+      ];
+
+      // When
+      let response;
+      var rPiece = reformPiece(piece)
+      console.log(rPiece);
       for (let i = 0; i < rPiece.length; i+=1) {
         response = await chai.request(app)
         .post(`${basePath}/homework/playing`)
