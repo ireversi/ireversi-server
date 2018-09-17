2ed72abf15393949122d2538717eacfaed0d674b
木村　幸介 <kosukekimura0528@gmail.com>
2018-09-17 21:57:18 +0900

    add No.2 test: turn over when sandwiched same pieces

--- src/routes/api/v1/kohski/playing.js
+++ src/routes/api/v1/kohski/playing.js
@@ -10,6 +10,21 @@ router.use((req, res, next) => {
   next();
 });
 
+const dirAll = [
+  [0, 1],
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+];
+
+function searchNext(pieces, nextX, nextY) {
+  return pieces.find(p => p.x === nextX && p.y === nextY);
+}
+
 router.route('/')
   .post(async (req, res) => {
     // piecesは多分盤面においてある全部のpiece
@@ -20,16 +35,56 @@ router.route('/')
       y: +req.body.y,
       userID: +req.body.userID,
     };
-
     // if the piece has already exist, return the original array
     if (pieces.find(p => p.x === result.x && p.y === result.y)) {
       res.json(pieces);
       return;
     }
 
+    // ----------------------------------------------
+    // 挟んだらめくれるテスト
+    const flip = [];
+
+    if (pieces.find(p => p.userID === result.userID)) {
+      for (let i = 0; i < dirAll.length; i += 1) {
+        const turnCandidate = [];
+
+        const dirX = dirAll[i][0];
+        const dirY = dirAll[i][1];
+        const toX = result.x + dirX;
+        const toY = result.y + dirY;
+
+        let dist = 1;
+        let dirPiece = pieces.find(p => p.x === toX && p.y === toY);
+
+        while (dirPiece !== undefined) {
+          if (dirPiece.userID !== result.userID) {
+            turnCandidate.push(dirPiece);
+            dist += 1;
+
+            const nextX = result.x + dirX * dist;
+            const nextY = result.y + dirY * dist;
+
+            dirPiece = searchNext(pieces, nextX, nextY);
+          } else if (dirPiece.userID === result.userID) {
+            for (let k = 0; k < turnCandidate.length; k += 1) {
+              if (turnCandidate[k] !== undefined) {
+                turnCandidate[k].userID = result.userID;
+                flip.push(turnCandidate[k]);
+              }
+            }
+            break;
+          }
+        }
+      }
+    }
+
     const Playing = new PlayingModel(result);
     await Playing.save();
+    await Promise.all(flip.map(p => PlayingModel.updateOne(
+      { x: p.x, y: p.y }, // 第一引数がfilter
+      { userID: p.userID }, // 第二引数でupdate
+    )));
     res.json(await PlayingModel.find({}, propFilter));
   });
-
 module.exports = router;
