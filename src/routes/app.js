ab01f7b484c24f53be0dfa674bc370aa7e0dc5e6
Shohei Kai <kai.entertainment0304@gmail.com>
2018-09-19 09:25:33 +0900

    fix: swagger files, add: static directry for images on swagger

--- src/routes/app.js
+++ src/routes/app.js
@@ -5,4 +5,6 @@ app.use(express.urlencoded({ extended: false }));// use:他のモジュールか
 app.use('/api/v1', require('./api/v1/index.js')); // 指定したURLがマッチしたら、index.jsに投げる
 app.use('/api/v2', require('./api/v2/index.js'));
 
+app.use('/', express.static('./static'));
+
 module.exports = app;
