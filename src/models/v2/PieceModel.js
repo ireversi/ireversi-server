2318c015a81d26beae9d2651b8fd796180340afe
Shohei Kai <kai.entertainment0304@gmail.com>
2018-09-24 23:49:58 +0900

    fix: index.js, PieceModel

--- src/models/v2/PieceModel.js
+++ src/models/v2/PieceModel.js
@@ -70,4 +70,75 @@ module.exports = {
     }
     return resultArray;
   },
+  sortList(list, sort) {
+    list.sort((a, b) => {
+      if (a.x < b.x) return -1 * sort.x;
+      if (a.x > b.x) return 1 * sort.x;
+      if (a.y < b.y) return -1 * sort.y;
+      if (a.y > b.y) return 1 * sort.y;
+      return 0; // for lint
+    });
+    return list;
+  },
+  checkList(list, key, result) {
+    const newList = [];
+    if (key === 'n' || key === 's') {
+      for (let i = 0; i < list.length; i += 1) {
+        if (i === 0) {
+          newList.push(list[i]);
+        } else if (list[i].userId !== result.userId
+          && Math.abs(list[i].y - list[i - 1].y) === 1) {
+          newList.push(list[i]);
+        } else if (list[i].userId === result.userId
+          && Math.abs(list[i].y - list[i - 1].y) === 1) {
+          return newList;
+        } else {
+          newList.length = 0;
+          return newList;
+        }
+      }
+    } else if (key === 'w' || key === 'e') {
+      for (let i = 0; i < list.length; i += 1) {
+        if (i === 0) {
+          newList.push(list[i]);
+        } else if (i > 0 && list[i].userId !== result.userId
+          && Math.abs(list[i].x - list[i - 1].x) === 1) {
+          newList.push(list[i]);
+        } else if (i > 0 && list[i].userId === result.userId
+          && Math.abs(list[i].x - list[i - 1].x) === 1) {
+          return newList;
+        } else {
+          newList.length = 0;
+          return newList;
+        }
+      }
+    } else {
+      for (let i = 0; i < list.length; i += 1) {
+        if (i === 0) {
+          newList.push(list[i]);
+        } else if (i > 0 && list[i].userId !== result.userId
+          && (Math.abs(list[i].x - list[i - 1].x) === 1)
+          && (Math.abs(list[i].y - list[i - 1].y) === 1)) {
+          newList.push(list[i]);
+        } else if (i > 0 && list[i].userId === result.userId
+          && (Math.abs(list[i].x - list[i - 1].x) === 1)
+          && (Math.abs(list[i].y - list[i - 1].y) === 1)) {
+          return newList;
+        } else {
+          newList.length = 0;
+          return newList;
+        }
+      }
+    }
+    return newList;
+  },
+  turnOverPiece(list, result) {
+    this.addPiece(result);
+    list.forEach((p) => {
+      if (p.userId !== result.userId) {
+        const updatePiece = { x: p.x, y: p.y, userId: result.userId };
+        this.updatePieces(updatePiece);
+      }
+    });
+  },
 };
