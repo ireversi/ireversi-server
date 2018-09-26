987fc8ff07c761e98307c159f96c7542cb0c7742
木村　幸介 <kosukekimura0528@gmail.com>
2018-09-27 06:58:19 +0900

    modified model case of board for displaying get size method

--- src/models/v2/boardController.js
+++ src/models/v2/boardController.js
@@ -24,8 +24,8 @@ function convertComparisonResult(result) {
 
 const result = [
   0, 0, 0, 0, 0,
-  0, 1, 2, 3, 0,
-  4, 5, 6, 7, 8,
+  0, 1, 2, 1, 0,
+  4, 5, 6, 7, 1,
   0, 9, 0, 0, 0,
   0, 0, 0, 0, 0,
 ];
