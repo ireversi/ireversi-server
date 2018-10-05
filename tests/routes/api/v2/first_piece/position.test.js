e409c8f91c85bc1082e9f511a361feb2e2120304
Shohei Kai <kai.entertainment0304@gmail.com>
2018-10-05 14:52:27 +0900

    last

--- tests/routes/api/v2/first_piece/position.test.js
+++ tests/routes/api/v2/first_piece/position.test.js
@@ -2,37 +2,26 @@ const chai = require('chai');
 const PieceStore = require('../../../../../src/models/v2/PieceStore.js');
 const app = require('../../../../../src/routes/app.js');
 
+const waitTime = PieceStore.getWaitTime();
+
 const basePath = '/api/v2/first_piece/position';
 
 describe('play', () => {
   // 盤面に自コマがない（１手目である）
-  // remaining 3500ミリ秒
-  // 返り値
-  // {
-  //   "status": true, // 置けたかどうか
-  //   "standby": {
-  //     "remaining": 0,
-  //     "piece": {
-  //       "x": 1,
-  //       "y": 1,
-  //       "user_id": 1
-  //     }
+  // 返り値の形式
+  // [
+  //   {
+  //     "status": true, // 置けたかどうか
+  //     "standby": {
+  //       "remaining": 0,
+  //       "piece": {
+  //         "x": 1,
+  //         "y": 1,
+  //         "user_id": 1
+  //       },
+  //     },
   //   }
-  // }
-
-  function array2Standby(array) {
-    const standbys = [];
-    const matchArray = PieceStore.array2Matchers(array);
-    for (let i = 0; i < matchArray.length; i += 1) {
-      const match = matchArray[i];
-      const standby = {
-        remaining: 0,
-        piece: match,
-      };
-      standbys.push(standby);
-    }
-    return standbys;
-  }
+  // ]
 
   it('start remaining timer', async () => {
     // Reset
@@ -42,38 +31,149 @@ describe('play', () => {
     const pieces = PieceStore.array2Pieces(
       [
         '1:1', '2:2',
-        0, 0,
+        '3:3', 0,
       ],
     );
 
-    const matches = array2Standby(
+    const matches = PieceStore.array2Standby(
       [
         1, 2,
-        0, 0,
+        3, 0,
       ],
     );
-    console.log(matches);
-
 
     // When
     let response;
     for (let i = 0; i < pieces.length; i += 1) {
+      const piece = pieces[i];
       response = await chai.request(app)
         .post(`${basePath}`)
+        .query({ userId: piece.userId })
         .set('content-type', 'application/x-www-form-urlencoded')
-        .send(pieces[i]);
+        .send({
+          x: piece.x,
+          y: piece.y,
+          userId: piece.userId,
+        });
     }
-    console.log(response.body);
-
 
     // Then
-    expect(response.body).toHaveLength(matches.length);
-    expect(response.body).toEqual(expect.arrayContaining(matches));
-    // check mongoDB result
-    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
-    // expect(pieceData).toHaveLength(rpieces.length);
-    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+    for (let i = 0; i < response.body.length; i += 1) {
+      const res = response.body[i];
+      const match = matches[i];
+      console.log(res);
+
+      const dateNow = Date.now(); // チェックする時刻
+      const timeLog = dateNow - res.standby.remaining;
+      const remaining = waitTime - timeLog;
+
+      expect(res.status).toEqual(match.status);
+      expect(remaining).toBeLessThanOrEqual(match.standby.remaining);
+      expect(res.standby.piece).toMatchObject(match.standby.piece);
+    }
   });
+
+  // すでにコマがある場合に置けない（piece/index.jsと同じロジック）
+  it('start remaining timer', async () => {
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = PieceStore.array2Pieces(
+      [
+        '1:1', ['2:2', '4:4'],
+        '3:3', 0,
+      ],
+    );
+    console.log(pieces);
+
+
+    const matches = PieceStore.array2Standby(
+      [
+        1, 2,
+        3, 0,
+      ],
+    );
+
+    // When
+    let response;
+    for (let i = 0; i < pieces.length; i += 1) {
+      const piece = pieces[i];
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send({
+          x: piece.x,
+          y: piece.y,
+          userId: piece.userId,
+        });
+    }
+
+    for (let i = 0; i < response.body.length; i += 1) {
+      const res = response.body[i];
+      const match = matches[i];
+      const dateNow = Date.now(); // チェックする時刻
+      const timeLog = dateNow - res.standby.remaining;
+      const remaining = waitTime - timeLog;
+
+      expect(res.status).toEqual(match.status);
+      expect(remaining).toBeLessThanOrEqual(match.standby.remaining);
+      expect(res.standby.piece).toMatchObject(match.standby.piece);
+    }
+  });
+
+  // // 盤面に自コマがある場合にremainingが回らないテスト
+  // it('start remaining timer', async () => {
+  //   await chai.request(app).delete(`${basePath}`);
+
+  //   // Given
+  //   const pieces = PieceStore.array2Pieces(
+  //     [
+  //       '1:1', '2:2', 0,
+  //       0, '3:3', 0,
+  //       0, '2:4', 0,
+  //     ],
+  //   );
+
+  //   const matches = PieceStore.array2Standby(
+  //     [
+  //       1, 2, 0,
+  //       0, 2, 0,
+  //       0, 2, 0,
+  //     ],
+  //   );
+
+  //   // When
+  //   let response;
+  //   for (let i = 0; i < pieces.length; i += 1) {
+  //     const piece = pieces[i];
+  //     response = await chai.request(app)
+  //       .post(`${basePath}`)
+  //       .query({ userId: piece.userId })
+  //       .set('content-type', 'application/x-www-form-urlencoded')
+  //       .send({
+  //         x: piece.x,
+  //         y: piece.y,
+  //         userId: piece.userId,
+  //       });
+  //   }
+
+  //   // Then
+  //   // expect(response.body).toHaveLength(matches.length);
+  //   // expect(response.body).toEqual(expect.arrayContaining(matches));
+
+  //   for (let i = 0; i < response.body.length; i += 1) {
+  //     const res = response.body[i];
+  //     const match = matches[i];
+  //     const dateNow = Date.now(); // チェックする時刻
+  //     const timeLog = dateNow - res.standby.remaining; //
+  //     const remaining = waitTime - timeLog;
+
+  //     expect(res.status).toEqual(match.status);
+  //     expect(remaining).toBeLessThanOrEqual(match.standby.remaining);
+  //     expect(res.standby.piece).toMatchObject(match.standby.piece);
+  //   }
+  // });
 });
 
 // 1つの値を送る
---
+++ tests/routes/api/v2/piece/sample.test.js
@@ -0,0 +1,554 @@
+
+const chai = require('chai');
+
+const ZERO0 = 0;
+
+const app = require('../../../../../src/routes/app.js');
+
+const basePath = '/api/v2/piece/';
+
+function convert2PieceRecord(pieces) {
+  const record = [];
+  for (let i = 0; i < pieces.length; i += 1) {
+    const size = Math.sqrt(pieces.length);
+    const piece = pieces[i];
+    if (piece !== 0 && !Array.isArray(piece)) {
+      const point = piece.indexOf(':');
+      const num = piece.slice(0, point);
+      const userId = piece.slice(point + 1);
+      const x = Math.floor(i % size);
+      const y = Math.floor(i / size);
+      record.push([num, x, y, userId]);
+    } else if (piece !== 0 && Array.isArray(piece)) {
+      for (let j = 0; j < piece.length; j += 1) {
+        const pie = piece[j];
+        const point = pie.indexOf(':');
+        const num = pie.slice(0, point);
+        const userId = pie.slice(point + 1);
+        const x = Math.floor(i % size);
+        const y = Math.floor(i / size);
+        record.push([num, x, y, userId]);
+      }
+    }
+  }
+  record.sort((a, b) => {
+    if (+a[0] < +b[0]) return -1;
+    if (+a[0] > +b[0]) return 1;
+    return 0;
+  });
+  return record;
+}
+
+function convertPiece(piece) {
+  const convert = { x: piece[1], y: piece[2], userId: piece[3] };
+  return convert;
+}
+
+function convertComparisonResult(result) {
+  const pieces = [];
+  const size = Math.sqrt(result.length);
+  for (let i = 0; i < result.length; i += 1) {
+    if (result[i] !== 0) {
+      const piece = {
+        x: Math.floor(i % size),
+        y: Math.floor(i / size),
+        userId: result[i],
+      };
+      pieces.push(piece);
+    }
+  }
+  return pieces;
+}
+
+
+describe('play', () => {
+  // 一つ駒を置く
+  it('put a piece', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      ZERO0, ZERO0,
+      '1:1', ZERO0,
+    ];
+
+    const result = [
+      0, 0,
+      1, 0,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    const rpieces = convertComparisonResult(result);
+
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 複数駒を置く
+  it('sets multi pieces', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      '1:1', '2:2',
+      ZERO0, ZERO0,
+    ];
+
+    const result = [
+      1, 2,
+      0, 0,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 同じところにおけないテスト
+  it('sets pieces same space', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      '1:1', ['2:2', '3:1'],
+      ZERO0, ZERO0,
+    ];
+
+    const result = [
+      1, 2,
+      0, 0,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+  //   // check mongoDB result
+  //   const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+  //   expect(pieceData).toHaveLength(rpieces.length);
+  //   expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 挟んだらめくれるテスト（左方向、上方向） part1
+  it('turn over piece about left and up', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      '1:1', '2:2', '3:1',
+      '4:2', ZERO0, ZERO0,
+      '5:1', ZERO0, ZERO0,
+    ];
+
+    const result = [
+      1, 1, 1,
+      1, 0, 0,
+      1, 0, 0,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // check mongoDB result
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 挟んだらめくれるテスト part2（右方向、下方向）
+  it('turn over piece about right and down', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      ZERO0, ZERO0, '5:1',
+      ZERO0, ZERO0, '4:2',
+      '3:1', '2:2', '1:1',
+    ];
+
+    const result = [
+      0, 0, 1,
+      0, 0, 1,
+      1, 1, 1,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // check mongoDB result
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 挟んだらめくれるテスト part3
+  it('turn over piece about upper left and lower left', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      ZERO0, ZERO0, '4:1', ZERO0,
+      ZERO0, '3:3', ZERO0, ZERO0,
+      '1:1', '2:2', '5:3', ZERO0,
+      ZERO0, ZERO0, ZERO0, '6:1',
+    ];
+
+    const result = [
+      0, 0, 1, 0,
+      0, 1, 0, 0,
+      1, 2, 1, 0,
+      0, 0, 0, 1,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // check mongoDB result
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 挟んだらめくれるテスト part4
+  it('turn over piece about upper right and lower right', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      ZERO0, '4:1', ZERO0, ZERO0,
+      ZERO0, ZERO0, '3:3', ZERO0,
+      ZERO0, '5:3', '2:2', '1:1',
+      '6:1', ZERO0, ZERO0, ZERO0,
+    ];
+
+    const result = [
+      0, 1, 0, 0,
+      0, 0, 1, 0,
+      0, 1, 2, 1,
+      1, 0, 0, 0,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // check mongoDB result
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 場に一枚も置いてない場合の駒置きテスト（反転無し） part1
+  it('in the case about nothing own piece, not turn over', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      ZERO0, '2:2', ZERO0, ZERO0,
+      ZERO0, '1:1', '4:1', ZERO0,
+      ZERO0, '3:3', ZERO0, ZERO0,
+      ZERO0, ZERO0, '5:4', ZERO0,
+    ];
+
+    const result = [
+      0, 2, 0, 0,
+      0, 1, 0, 0,
+      0, 3, 0, 0,
+      0, 0, 0, 0,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // check mongoDB result
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 場に一枚も置いてない場合の駒置きテスト（反転有り） part2
+  it('the case about nothing own piece, to turn over', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      ZERO0, '2:2', ZERO0, ZERO0,
+      ZERO0, '1:1', '5:2', ZERO0,
+      ZERO0, '3:3', ZERO0, ZERO0,
+      ZERO0, '4:1', '6:3', ZERO0,
+    ];
+
+    const result = [
+      0, 2, 0, 0,
+      0, 1, 0, 0,
+      0, 1, 0, 0,
+      0, 1, 3, 0,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // check mongoDB result
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 場に駒がある場合の駒置きテスト（上下左右チェック） part1
+  it('the case about exist own piece, check to turn over four direction', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      '20:1', '21:1', '5:1', '6:1', '7:1',
+      '19:1', ZERO0, '2:2', ZERO0, '8:1',
+      '18:1', '17:5', '1:1', '3:3', '9:1',
+      '16:1', ZERO0, '4:4', ZERO0, '10:1',
+      '15:1', '14:1', '13:1', '12:1', '11:1',
+    ];
+
+    const result = [
+      0, 0, 1, 0, 0,
+      0, 0, 1, 0, 0,
+      1, 1, 1, 1, 1,
+      0, 0, 1, 0, 0,
+      0, 0, 1, 0, 0,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // // check mongoDB result
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+
+  // 場に駒がある場合の駒置きテスト（斜めの4方向チェック） part2
+  it('the case about exist own piece, check to turn over four direction', async () => {
+    // Reset
+    await chai.request(app).delete(`${basePath}`);
+
+    // Given
+    const pieces = [
+      '18:1', ZERO0, ZERO0, ZERO0, '10:1',
+      ZERO0, '9:9', '2:2', '6:6', '11:1',
+      '17:1', '5:5', '1:1', '3:3', '12:1',
+      '16:1', '8:8', '4:4', '7:7', '13:1',
+      '15:1', ZERO0, ZERO0, ZERO0, '14:1',
+    ];
+
+    const result = [
+      1, 0, 0, 0, 1,
+      0, 1, 2, 1, 0,
+      1, 1, 1, 1, 1,
+      0, 1, 4, 1, 0,
+      1, 0, 0, 0, 1,
+    ];
+
+    // When
+    let response;
+    const record = convert2PieceRecord(pieces);
+
+    for (let i = 0; i < record.length; i += 1) {
+      let piece = record[i];
+      piece = convertPiece(piece);
+      response = await chai.request(app)
+        .post(`${basePath}`)
+        .query({ userId: piece.userId })
+        .set('content-type', 'application/x-www-form-urlencoded')
+        .send(piece);
+    }
+
+    // Then
+    // check express result
+    const rpieces = convertComparisonResult(result);
+    expect(response.body).toHaveLength(rpieces.length);
+    expect(response.body).toEqual(expect.arrayContaining(rpieces));
+
+    // // check mongoDB result
+    // const pieceData = JSON.parse(JSON.stringify(await PieceModel.find({}, propfilter)));
+    // expect(pieceData).toHaveLength(rpieces.length);
+    // expect(pieceData).toEqual(expect.arrayContaining(rpieces));
+  });
+});
