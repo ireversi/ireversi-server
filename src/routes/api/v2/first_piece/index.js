37b3268a32ad01e44bca6d0ad0d8bfcc6ca2283a
木村　幸介 <kosukekimura0528@gmail.com>
2018-10-06 09:00:48 +0900

    modified comment out in first piece index.js

--- src/routes/api/v2/first_piece/index.js
+++ src/routes/api/v2/first_piece/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-// router.use('/direction', require('./direction.js'));
-// router.use('/position', require('./position.js'));
+router.use('/direction', require('./direction.js'));
+router.use('/position', require('./position.js'));
 
 module.exports = router;
