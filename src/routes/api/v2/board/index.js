43b285c74206575f8f68450e3222c1d775416567
木村　幸介 <kosukekimura0528@gmail.com>
2018-10-08 09:23:49 +0900

    restore CORS

--- src/routes/api/v2/board/index.js
+++ src/routes/api/v2/board/index.js
@@ -6,6 +6,13 @@ const calcScore = require('./calcScore.js');
 const calcSize = require('./calcSize.js');
 router.use('/specified_range', require('./specified_size.js'));
 
+// for CORS
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 router.route('/').get(async (req, res) => {
   // userIdを取得
   const userId = +req.query.userId;
