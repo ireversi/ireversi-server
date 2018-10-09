010e9f2e8fdea4ef9fc6911ed29ea95f3b471374
木村　幸介 <kosukekimura0528@gmail.com>
2018-10-09 22:13:31 +0900

    commit before merging upstream

--- src/routes/api/v2/index.js
+++ src/routes/api/v2/index.js
@@ -12,4 +12,28 @@ router.use('/board/specified_size', require('./board/specified_size.js'));
 router.use('/piece', require('./piece/index.js'));
 router.use('/first_piece', require('./first_piece/index.js'));
 
+// var config = {
+//   appRoot:'../../../config.js' // required config
+// };
+
+// var jwt_decode = require('jwt-decode');
+
+// config.swaggerSecurityHandlers = {
+//   tokenAuth : function (req, authOrSecDef, scopesOrApiKey, cb) {
+//     try{
+//       var decoded = jwt_decode(scopesOrApiKey);
+
+//       req.requestContext = {
+//         authorizer : {
+//           claims : decoded
+//         }
+//       };
+
+//       cb();
+//     }catch(error){
+//       cb(error);
+//     }
+//   }
+// };
+
 module.exports = router;
