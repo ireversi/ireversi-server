14ab48fbd7bbd932cbf1dff5758e2a481c1dc5b8
木村　幸介 <kosukekimura0528@gmail.com>
2018-10-16 07:46:37 +0900

    add userAuth on swagger/add eplorer option on swaggerSpecs/add adcess test

--- src/routes/api/v2/index.js
+++ src/routes/api/v2/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const jwt = require('jsonwebtoken');
 
 // for CORS
 router.use((req, res, next) => {
@@ -8,16 +8,56 @@ router.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
-// accessTokenの確認とルーティング
-// router.use((req,res,next)=>{
-//   const accessToken = req.headers.body;
-//   console.log(accessToken);
-// })
-router.use('/board', require('./board/index.js'));
+
+// userIdの取得はいつでもできるようにする。
+router.use('/user_id_generate', require('./userIdGenerate/index.js'));
+
+// authorizationに対応するコード
+const config = {
+  appRoot: __dirname // required config
+};
+config.swaggerSecurityHandlers = {
+  tokenAuth : function (req, authOrSecDef, scopesOrApiKey) {
+    try{
+      var decoded = jwt.decode(scopesOrApiKey);
+      req.requestContext = {
+        authorizer:{
+          userId : decoded
+        }
+      };
+      // cb();
+    }catch(error){
+      // cb(error);
+    }
+  }
+};
+
+
+router.use('/access_auth', (req, res, next) => {
+  try {
+    let status = '';
+    const headerValue = req.headers.accesstoken;
+    const accessToken = jwt.decode(headerValue);
+    const isUserId = (Object.keys(accessToken).indexOf('userId') !== -1);
+    if (isUserId) {
+      const headerValue = req.headers.accesstoken;
+      const accessToken = jwt.decode(headerValue);
+      const userIdParse = accessToken.userId;
+      console.log(userIdParse);
+      console.log(req.headers)
+      status = 'Success';
+      next();
+    } else {
+      status = 'Error';
+      res.status(401).end();
+    }
+  } catch (e) {
+    res.status(401).end();
+  }
+});
+router.use('/board',require('./board/index.js'));
 router.use('/board/specified_size', require('./board/specified_size.js'));
 router.use('/piece', require('./piece/index.js'));
 router.use('/first_piece', require('./first_piece/index.js'));
-router.use('/userIdGenerate', require('./userIdGenerate/index.js'));
-router.use('/accessTest', require('./accessTest/index.js'));
 
 module.exports = router;
--- src/routes/app.js
+++ src/routes/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('passport');
+// const LocalStrategy = require('passport-local').Strategy;
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));// use:他のモジュールから飛んでくる情報をparseする
