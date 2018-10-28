84db66b5628547a560aa521bbb7d1a3c7415f8da
木村　幸介 <kosukekimura0528@gmail.com>
2018-10-29 07:48:21 +0900

    modified generatetoken: return token and decoded userId

--- src/routes/api/v2/userIdGenerate/index.js
+++ src/routes/api/v2/userIdGenerate/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
 const generateToken = require('./generateToken');
 
 router.route('/').post((req, res) => {
   const accessToken = generateToken.generate();
-  res.json({ accessToken });
+  const { userId } = jwt.decode(accessToken);
+  res.json({ accessToken, userId });
 });
 
 module.exports = router;
