5003418f7704d5444d1aa9b82b3f3c6a55ee371b
木村　幸介 <kosukekimura0528@gmail.com>
2018-11-03 05:29:52 +0900

    modified userModel.js

--- src/models/v2/UserModel.js
+++ src/models/v2/UserModel.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-  userId: String,
-  username: String,
+  created: Date,
+  modified: Date,
+  user_id: Number,
 });
 
 module.exports = mongoose.model('UserModel', UserSchema);
